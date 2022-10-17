efd4aab53d85c0c7a976c8b79c1a1ce46c5dcd26
CalebColby <ccolby@student.neumont.org>
2022-10-17 13:49:36 -0600

    added

--- stock_lab/src/main/java/com/example/App.java
+++ stock_lab/src/main/java/com/example/App.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
@@ -10,8 +14,25 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws FileNotFoundException
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws FileNotFoundException {
+        String jsonData = GetJsonDataAsString("C:\\Users\\Ccolby\\OneDrive - Neumont College of Computer Science\\Desktop\\stock_transations-3.by.account.holder.json");
+        
+
+    }
+
+    private static String GetJsonDataAsString(String filePath) throws FileNotFoundException{
+        File file = new File(filePath);
+        Scanner sc = new Scanner(file);
+
+        StringBuilder sb = new StringBuilder();
+
+        while(sc.hasNextLine()){
+            sb.append(sc.nextLine());
+        }
+        sc.close();
+        
+        return sb.toString();
     }
 }
